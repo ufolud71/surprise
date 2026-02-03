369228798ec95d43847a85d0c8d808f386298f47
ufolud71 <54034193+ufolud71@users.noreply.github.com>
2026-02-03 17:43:16 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+from js import document
+
+scale = 1.0
+
+yes = document.getElementById("yes")
+no  = document.getElementById("no")
+msg = document.getElementById("msg")
+q   = document.getElementById("q")
+
+def on_no(event=None):
+    global scale
+    scale *= 1.35
+    yes.style.fontSize = f"{22 * scale}px"
+    yes.style.padding = f"{12 * scale}px {22 * scale}px"
+
+    if scale > 6:
+        msg.innerText = "Ups… chyba jednak TAK 😄"
+        no.disabled = True
+        yes.style.width = "90%"
+        yes.style.height = "180px"
+
+def on_yes(event=None):
+    q.innerText = "Yay!! 💘💘💘"
+    msg.innerText = "To randka! 😍"
+    yes.disabled = True
+    no.disabled = True
+
+yes.addEventListener("click", on_yes)
+no.addEventListener("click", on_no)
