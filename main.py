1cb83231322c3ac8cdf6f872f08921ea1826a14f
ufolud71 <54034193+ufolud71@users.noreply.github.com>
2026-02-03 18:04:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,29 @@
-from pyscript import document, when
+from browser import document
 
 scale = 1.0
 
-@when("click", "#no")
-def on_no(event):
+yes = document["yes"]
+no  = document["no"]
+msg = document["msg"]
+q   = document["q"]
+
+def on_no(ev):
     global scale
     scale *= 1.35
-
-    yes = document.querySelector("#yes")
-    msg = document.querySelector("#msg")
-    no  = document.querySelector("#no")
-
     yes.style.fontSize = f"{22 * scale}px"
     yes.style.padding  = f"{12 * scale}px {22 * scale}px"
 
     if scale > 6:
-        msg.innerText = "Ups… chyba jednak TAK 😄"
+        msg.text = "Ups… chyba jednak TAK 😄"
         no.disabled = True
         yes.style.width = "90%"
         yes.style.height = "180px"
 
-@when("click", "#yes")
-def on_yes(event):
-    q   = document.querySelector("#q")
-    msg = document.querySelector("#msg")
-    yes = document.querySelector("#yes")
-    no  = document.querySelector("#no")
-
-    q.innerText = "Yay!! 💘💘💘"
-    msg.innerText = "To randka! 😍"
+def on_yes(ev):
+    q.text = "Yay!! 💘💘💘"
+    msg.text = "To randka! 😍"
     yes.disabled = True
     no.disabled = True
+
+no.bind("click", on_no)
+yes.bind("click", on_yes)
